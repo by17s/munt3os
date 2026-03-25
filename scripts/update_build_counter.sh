4440272b509feb25271a119a48158931be4e625e
by17s <give.56.com@gmail.com>
2026-03-26 00:34:38 +0500

    Update version

--- scripts/update_build_counter.sh
+++ scripts/update_build_counter.sh
@@ -34,10 +34,10 @@ cat > "$HEADER_FILE" <<EOF
 #define BUILD_DATE "$DATE"
 
 #define OS_NAME "MountainOS"
-#define OS_VERSION "3.1.1"
+#define OS_VERSION "3.1.2"
 
 #define OS_KRNL_NAME "Munt3"
-#define OS_KRNL_VERSION "3.1.1"
+#define OS_KRNL_VERSION "3.1.2"
 
 #endif /* BUILD_H */
 EOF
